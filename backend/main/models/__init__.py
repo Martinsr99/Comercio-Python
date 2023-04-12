e7638b2ecfbb091cc445de7c60b3f8cd935e18f4
Martinsr99 <silesreche.martin@gmail.com>
2023-04-12 14:57:58 +0200

    feat: Init project and add get/post endpoints

--- backend/main/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Usuario import Usuario as UsuarioModel
-from .Producto import Producto as ProductoModel
-from .Compra import Compra as CompraModel
-from .ProductoCompra import ProductoCompra as ProductoCompraModel
